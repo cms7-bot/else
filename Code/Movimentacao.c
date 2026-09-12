29104c1eac8ea9eea694ec18de49e0e52c0c571d
Igor França <igorluizalmeidafranca@gmail.com>
2026-09-11 21:01:35 -0300

    Adiciona de logo provisória com animação

--- Code/Movimentacao.c
+++ Code/Movimentacao.c
@@ -1,17 +1,14 @@
 #include "raylib.h"
 
-
 int telaInicio(Texture2D botaoJogar, RenderTexture2D telaVirtual, float escala, float offsetX, float offsetY);
 int telaAviso(RenderTexture2D telaVirtual, float escala, float offsetX, float offsetY);
 
 int main()
-{   
-
+{
     int larguraResolucao = 1280;
     int alturaResolucao = 720;
 
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-
     InitWindow(larguraResolucao, alturaResolucao, "else");
     SetTargetFPS(60);
     InitAudioDevice();
@@ -22,12 +19,13 @@ int main()
     Music trilhaSonora2 = LoadMusicStream("musicas/JazzTrilha2.mp3");
     Music EscritorioTrilhaSonora = LoadMusicStream("musicas/Escritorio2.mp3");
     Sound somDePassos = LoadSound("EfeitosSonoros/Passos2.mp3");
+
     SetMusicVolume(trilhaSonora2, 0.05f);
     SetMusicVolume(trilhaSonora1, 0.10f);
     SetMusicVolume(EscritorioTrilhaSonora, 0.2f);
+
     PlayMusicStream(trilhaSonora1);
     PlayMusicStream(EscritorioTrilhaSonora);
-
 
     Texture2D fundoCeramica = LoadTexture("cenario/ceramica.png");
 
@@ -48,11 +46,16 @@ int main()
     Texture2D esquerda3 = LoadTexture("assets/LadoEsquerdo/BonecoAndandoLadoEsquerdo2.png");
 
     Texture2D botaoJogar = LoadTexture("Botoes/BotaoJogar.png");
+    Texture2D logoInicio = LoadTexture("cenario/logoInicio.png");
 
-    int tela = 0;
+    int tela = -1;
 
-    float x = (larguraResolucao - frente1.width)/2;
-    float y = (alturaResolucao - frente1.height)/2;
+    float tempoIntro = 0.0f;
+    float tempoLogo = 2.0f;
+    float tempoFade = 1.0f;
+
+    float x = (larguraResolucao - frente1.width) / 2;
+    float y = (alturaResolucao - frente1.height) / 2;
 
     int velocidade = 5;
     int direcao = 0;
@@ -62,29 +65,107 @@ int main()
 
     while (!WindowShouldClose())
     {
-
         float escalaX = (float)GetScreenWidth() / larguraResolucao;
         float escalaY = (float)GetScreenHeight() / alturaResolucao;
-
         float escala;
 
         if (escalaX < escalaY)
-        {
             escala = escalaX;
-        }
         else
-        {
             escala = escalaY;
-        }
 
         float larguraFinal = larguraResolucao * escala;
         float alturaFinal = alturaResolucao * escala;
 
         float offsetX = (GetScreenWidth() - larguraFinal) / 2;
         float offsetY = (GetScreenHeight() - alturaFinal) / 2;
-        
+
+        if (tela == -1)
+        {
+            tempoIntro += GetFrameTime();
+
+            float alpha = 0.0f;
+
+            if (tempoIntro >= tempoLogo)
+            {
+                alpha = (tempoIntro - tempoLogo) / tempoFade;
+
+                if (alpha > 1.0f)
+                    alpha = 1.0f;
+            }
+
+            BeginTextureMode(telaVirtual);
+            ClearBackground(BLACK);
+
+            float escalaLogoX = (larguraResolucao * 0.55f) / logoInicio.width;
+            float escalaLogoY = (alturaResolucao * 0.75f) / logoInicio.height;
+            float escalaLogo;
+
+            if (escalaLogoX < escalaLogoY)
+                escalaLogo = escalaLogoX;
+            else
+                escalaLogo = escalaLogoY;
+
+            float larguraLogo = logoInicio.width * escalaLogo;
+            float alturaLogo = logoInicio.height * escalaLogo;
+
+            DrawTextureEx(
+                logoInicio,
+                (Vector2){
+                    (larguraResolucao - larguraLogo) / 2,
+                    (alturaResolucao - alturaLogo) / 2
+                },
+                0,
+                escalaLogo,
+                WHITE
+            );
+
+            DrawRectangle(
+                0,
+                0,
+                larguraResolucao,
+                alturaResolucao,
+                Fade(BLACK, alpha)
+            );
+
+            EndTextureMode();
+
+            BeginDrawing();
+            ClearBackground(BLACK);
+
+            Rectangle origemIntro = {
+                0,
+                0,
+                (float)telaVirtual.texture.width,
+                -(float)telaVirtual.texture.height
+            };
+
+            Rectangle destinoIntro = {
+                offsetX,
+                offsetY,
+                larguraFinal,
+                alturaFinal
+            };
+
+            DrawTexturePro(
+                telaVirtual.texture,
+                origemIntro,
+                destinoIntro,
+                (Vector2){0, 0},
+                0,
+                WHITE
+            );
+
+            EndDrawing();
+
+            if (tempoIntro >= tempoLogo + tempoFade)
+                tela = 0;
+
+            continue;
+        }
+
         if (tela == 0)
-        {   
+        {
             UpdateMusicStream(trilhaSonora1);
 
             if (telaInicio(botaoJogar, telaVirtual, escala, offsetX, offsetY) == 1)
@@ -93,34 +174,34 @@ int main()
                 PlayMusicStream(EscritorioTrilhaSonora);
                 StopMusicStream(trilhaSonora1);
             }
-            
+
             continue;
         }
 
-        if (tela==1){
+        if (tela == 1)
+        {
+            UpdateMusicStream(EscritorioTrilhaSonora);
 
-        UpdateMusicStream(EscritorioTrilhaSonora);
+            if (telaAviso(telaVirtual, escala, offsetX, offsetY) == 2)
+            {
+                tela = 2;
+                StopMusicStream(EscritorioTrilhaSonora);
+                PlayMusicStream(trilhaSonora2);
+            }
 
-        if (telaAviso(telaVirtual, escala, offsetX, offsetY)==2)
-        {    
-        tela = 2;
-        StopMusicStream(EscritorioTrilhaSonora);
-        PlayMusicStream(trilhaSonora2);}
-        continue;
+            continue;
         }
 
-        if (tela == 2) {
-        UpdateMusicStream(trilhaSonora2);
-        }
-
+        if (tela == 2)
+            UpdateMusicStream(trilhaSonora2);
 
         andando = 0;
 
         if (IsKeyDown(KEY_W))
         {
-        if (!IsSoundPlaying(somDePassos)){
-        PlaySound(somDePassos);
-        }
+            if (!IsSoundPlaying(somDePassos))
+                PlaySound(somDePassos);
+
             y -= velocidade;
             direcao = 1;
             andando = 1;
@@ -128,9 +209,9 @@ int main()
 
         if (IsKeyDown(KEY_S))
         {
-        if (!IsSoundPlaying(somDePassos)){
-        PlaySound(somDePassos);
-        }
+            if (!IsSoundPlaying(somDePassos))
+                PlaySound(somDePassos);
+
             y += velocidade;
             direcao = 0;
             andando = 1;
@@ -138,9 +219,9 @@ int main()
 
         if (IsKeyDown(KEY_A))
         {
-        if (!IsSoundPlaying(somDePassos)){
-        PlaySound(somDePassos);
-        }
+            if (!IsSoundPlaying(somDePassos))
+                PlaySound(somDePassos);
+
             x -= velocidade;
             direcao = 2;
             andando = 1;
@@ -148,9 +229,9 @@ int main()
 
         if (IsKeyDown(KEY_D))
         {
-        if (!IsSoundPlaying(somDePassos)){
-        PlaySound(somDePassos);
-        }
+            if (!IsSoundPlaying(somDePassos))
+                PlaySound(somDePassos);
+
             x += velocidade;
             direcao = 3;
             andando = 1;
@@ -166,9 +247,7 @@ int main()
                 tempo = 0;
 
                 if (frame >= 3)
-                {
                     frame = 0;
-                }
             }
         }
         else
@@ -177,7 +256,6 @@ int main()
             tempo = 0;
         }
 
-        // ---- Seleciona a textura atual com base na direção e no frame ----
         Texture2D texturaAtual;
 
         if (direcao == 0)
@@ -205,24 +283,22 @@ int main()
             else texturaAtual = direita3;
         }
 
-        // ---- LIMITES DE TELA (usando o tamanho da textura atual) ----
         if (x < 0) x = 0;
         if (y < 0) y = 0;
-        if (x > larguraResolucao - texturaAtual.width) x = larguraResolucao - texturaAtual.width;
-        if (y > alturaResolucao - texturaAtual.height) y = alturaResolucao - texturaAtual.height;
-        // --------------------------------------------------------------
 
+        if (x > larguraResolucao - texturaAtual.width)
+            x = larguraResolucao - texturaAtual.width;
+
+        if (y > alturaResolucao - texturaAtual.height)
+            y = alturaResolucao - texturaAtual.height;
 
         BeginTextureMode(telaVirtual);
 
         ClearBackground(RAYWHITE);
-
         DrawTexture(fundoCeramica, 0, 0, WHITE);
-
         DrawTexture(texturaAtual, x, y, WHITE);
 
         EndTextureMode();
-
 
         BeginDrawing();
 
@@ -271,10 +347,11 @@ int main()
     UnloadTexture(esquerda3);
 
     UnloadTexture(botaoJogar);
+    UnloadTexture(logoInicio);
 
     UnloadRenderTexture(telaVirtual);
 
     CloseWindow();
-    
+
     return 0;
 }
